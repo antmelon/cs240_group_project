5e89d59bea8e9151230fd30ab69478b0e930b88d
Anthony Longo <ferrari13@me.com>
2020-04-28 13:49:41 -0400

    added main

---
+++ main.cc
@@ -0,0 +1,13 @@
+
+typename Coords <int, int>;
+
+int main(int argc, char * argv[]){
+   Coords coordinates[];
+   //pass random coordinates to array
+   //
+   //Initiialize Quickhull and Giftwrapping
+   //
+   //Print out hull coordinates/time for quickhull
+   //
+   //Print out hull coordinates/time for giftwrapping
+}
