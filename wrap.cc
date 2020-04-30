daebab410382b5e6d40e30b9ae32f498bc4d04af
sblack3 <sblack3@binghamton.edu>
2020-04-30 02:07:52 -0400

    added some basline code to the wrapper

--- wrap.cc
+++ wrap.cc
@@ -5,10 +5,19 @@ using namespace std;
 
 #define iPair pair<int, int>
 
-// Do following while we don’t come back to the first (or leftmost) point.
-// The next point q is the point such that the triplet (p, q, r) is counterclockwise for any other point r.
-// next[p] = q (Store q as next of p in the output convex hull).
-// p = q (Set p as q for next iteration).
+
+int isClockwise(iPair p, iPair q, iPair r){
+     int val = (q.second - p.second) * (r.first - q.first) - 
+              (q.first - p.first) * (r.second - q.second); 
+  
+    if (val == 0) return 0;  // colinear 
+    return (val > 0)? 1: 2;
+}
+//p = leftmost point (cords can just be whatever point is the most negative x)
+//prime = p
+//find a point q that seems like it should be the next coordinate point
+// create algorithm to determine when p and q are counter clockwise from the rest of the data points
+//repeat this process until you return to prime
 
 
 //just setting this up for future refernce, will add more to it in the future
